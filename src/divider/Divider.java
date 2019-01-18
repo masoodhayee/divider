85c383d880d7dcd1e7f7ec6d840b1e67348b7167
masood <masood@192.168.43.246>
2019-01-18 15:03:44 +0300

    first commit

---
+++ src/divider/Divider.java
@@ -0,0 +1,17 @@
+
+package divider;
+
+public class Divider {
+
+    
+    public static void main(String[] args) {
+
+        int s =100;
+        for (int i=1 ; i<=s ; i++){
+            if(s%i == 0 ){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
